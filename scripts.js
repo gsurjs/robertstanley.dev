674da9e3c85595d42939d16547ce4f19636a5186
gsurjs <155509989+gsurjs@users.noreply.github.com>
2026-02-18 19:39:24 -0500

    adding logic check to openModal -panthermarket ? overlay : iframe

--- scripts.js
+++ scripts.js
@@ -141,18 +141,27 @@ function openModal(title) {
     urlBar.textContent = p.url;
 
     if (p.type === 'web') {
-        modalBody.innerHTML = `
-            <div class="iframe-wrapper">
+        // CHECK: Is PantherMarket?
+        if (pKey === 'panther') {
+            // YES: Show overlay so user clicks through
+            modalBody.innerHTML = `
+                <div class="iframe-wrapper">
+                    <iframe src="${p.url}" class="preview-frame" sandbox="allow-scripts allow-same-origin allow-forms"></iframe>
+                    
+                    <a href="${p.url}" target="_blank" class="iframe-overlay">
+                        <div class="overlay-msg">
+                            <span>Interact on Live Site</span>
+                            <svg class="icon" style="width: 20px; height: 20px; color: #000;" viewBox="0 0 24 24"><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/></svg>
+                        </div>
+                    </a>
+                </div>
+            `;
+        } else {
+            // NO, Show interactive iframe directly
+            modalBody.innerHTML = `
                 <iframe src="${p.url}" class="preview-frame" sandbox="allow-scripts allow-same-origin allow-forms"></iframe>
-                
-                <a href="${p.url}" target="_blank" class="iframe-overlay">
-                    <div class="overlay-msg">
-                        <span>Interact on Live Site</span>
-                        <svg class="icon" style="width: 20px; height: 20px; color: #000;" viewBox="0 0 24 24"><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/></svg>
-                    </div>
-                </a>
-            </div>
-        `;
+            `;
+        }
     } else {
         modalBody.innerHTML = `
             <div class="repo-card">
